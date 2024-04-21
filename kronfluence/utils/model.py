9c2128bd43cf563566423f8bf7e75995f6cf19f9
xeon27 <omkar.dige@vectorinstitute.ai>
2024-04-21 00:16:50 -0400

    Add more args for fsdp helper func

--- kronfluence/utils/model.py
+++ kronfluence/utils/model.py
@@ -1,10 +1,19 @@
+from typing import Optional
 import functools
+
 import torch
+from torch import nn
 import torch.distributed as dist
 
-from torch.distributed.fsdp import CPUOffload
-from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
-from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
+from torch.distributed.fsdp import (
+    FullyShardedDataParallel as FSDP, 
+    CPUOffload, 
+    ShardingStrategy,
+)
+from torch.distributed.fsdp.wrap import (
+    size_based_auto_wrap_policy, 
+    transformer_auto_wrap_policy
+)
 from torch.nn.parallel.distributed import DistributedDataParallel
 
 
@@ -46,6 +55,10 @@ def apply_fsdp(
     local_rank: int,
     rank: int,
     world_size: int,
+    sharding_strategy: str = "FULL_SHARD",
+    cpu_offload: bool = True,
+    is_transformer: bool = False,
+    layer_to_wrap: Optional[nn.Module] = None,
 ) -> FSDP:
     """
     Applies FullyShardedDataParallel (FSDP) to the given model.
@@ -55,6 +68,16 @@ def apply_fsdp(
         local_rank (int): The local rank of the current process.
         rank (int): The rank of the current process.
         world_size (int): The total number of processes.
+        sharding_strategy (str): The sharding strategy to use. 
+            Defaults to "FULL_SHARD".
+        cpu_offload (bool): Whether to offload parameters to CPU. Check 
+            https://pytorch.org/docs/2.2/fsdp.html#torch.distributed.fsdp.CPUOffload. 
+            Defaults to True.
+        is_transformer (bool): Whether the model is a transformer model. 
+            Defaults to False.
+        layer_to_wrap (nn.Module, optional): The specific layer to wrap 
+            for transformer models. Required if `is_transformer` is True. 
+            Defaults to None.
 
     Returns:
         FSDP: The model wrapped with FSDP.
@@ -63,13 +86,28 @@ def apply_fsdp(
     device = torch.device("cuda:{}".format(local_rank))
     torch.cuda.set_device(local_rank)
 
-    my_auto_wrap_policy = functools.partial(
-        size_based_auto_wrap_policy, min_num_params=100
-    )
+    if not hasattr(ShardingStrategy, sharding_strategy):
+        msg = f"The provided sharding strategy {sharding_strategy} does not exist."
+        raise ValueError(msg)
+    sharding_strategy = getattr(ShardingStrategy, sharding_strategy)
+    if is_transformer:
+        if layer_to_wrap is None:
+            raise ValueError("`layer_to_wrap` must be provided for transformer models.")
+        my_auto_wrap_policy = functools.partial(
+            transformer_auto_wrap_policy, 
+            transformer_layer_cls={layer_to_wrap},
+        )
+    else:
+        my_auto_wrap_policy = functools.partial(
+            size_based_auto_wrap_policy, 
+            min_num_params=100,
+        )
+
     fsdp_cfg = {
         "use_orig_params": False,
+        "sharding_strategy": sharding_strategy,
         "auto_wrap_policy": my_auto_wrap_policy,
-        "cpu_offload": CPUOffload(offload_params=True),
+        "cpu_offload": CPUOffload(offload_params=cpu_offload),
     }
 
     model = model.to(device=device)
