833864b73a6fbf9009bf0c53f5c5667da5813f0f
xeon27 <omkar.dige@vectorinstitute.ai>
2024-04-20 23:19:54 -0400

    Add fsdp helper func

--- kronfluence/utils/model.py
+++ kronfluence/utils/model.py
@@ -1,5 +1,10 @@
+import functools
 import torch
 import torch.distributed as dist
+
+from torch.distributed.fsdp import CPUOffload
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
 from torch.nn.parallel.distributed import DistributedDataParallel
 
 
@@ -32,5 +37,42 @@ def apply_ddp(
 
     model = model.to(device=device)
     model = DistributedDataParallel(model, **ddp_cfg)
+
+    return model
+
+
+def apply_fsdp(
+    model: torch.nn.Module,
+    local_rank: int,
+    rank: int,
+    world_size: int,
+) -> FSDP:
+    """
+    Applies FullyShardedDataParallel (FSDP) to the given model.
+
+    Args:
+        model (torch.nn.Module): The model to apply FSDP to.
+        local_rank (int): The local rank of the current process.
+        rank (int): The rank of the current process.
+        world_size (int): The total number of processes.
+
+    Returns:
+        torch.nn.Module: The model wrapped with FSDP.
+    """
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    device = torch.device("cuda:{}".format(local_rank))
+    torch.cuda.set_device(local_rank)
+
+    my_auto_wrap_policy = functools.partial(
+        size_based_auto_wrap_policy, min_num_params=100
+    )
+    fsdp_cfg = {
+        "use_orig_params": False,
+        "auto_wrap_policy": my_auto_wrap_policy,
+        "cpu_offload": CPUOffload(offload_params=True),
+    }
+
+    model = model.to(device=device)
+    model = FSDP(model, **fsdp_cfg)
 
     return model
