97ad305ed729b10fc1039c5ed278beb742ba0177
Juhan Bae <pomonam15@gmail.com>
2024-06-11 01:28:00 -0400

    Fix documentation

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -85,6 +85,8 @@ def test_default_factor_arguments() -> None:
 
     assert factor_args.strategy == "ekfac"
     assert factor_args.use_empirical_fisher is False
+    assert factor_args.distributed_sync_steps == 1000
+    assert factor_args.amp_dtype is None
 
     assert factor_args.covariance_max_examples == 100_000
     assert factor_args.covariance_data_partition_size == 1
@@ -102,19 +104,22 @@ def test_default_factor_arguments() -> None:
 
 
 def test_default_score_arguments() -> None:
-    factor_args = ScoreArguments()
+    score_args = ScoreArguments()
 
-    assert factor_args.damping == 1e-08
-    assert factor_args.cached_activation_cpu_offload is False
+    assert score_args.damping == 1e-08
+    assert score_args.cached_activation_cpu_offload is False
+    assert score_args.distributed_sync_steps == 1000
+    assert score_args.amp_dtype is None
 
-    assert factor_args.data_partition_size == 1
-    assert factor_args.module_partition_size == 1
-    assert factor_args.per_module_score is False
+    assert score_args.data_partition_size == 1
+    assert score_args.module_partition_size == 1
+    assert score_args.per_module_score is False
 
-    assert factor_args.query_gradient_rank is None
-    assert factor_args.num_query_gradient_aggregations == 1
-    assert factor_args.query_gradient_svd_dtype == torch.float32
+    assert score_args.query_gradient_rank is None
+    assert score_args.num_query_gradient_aggregations == 1
+    assert score_args.query_gradient_svd_dtype == torch.float32
+    assert score_args.use_measurement_for_self_influence is False
 
-    assert factor_args.score_dtype == torch.float32
-    assert factor_args.per_sample_gradient_dtype == torch.float32
-    assert factor_args.precondition_dtype == torch.float32
+    assert score_args.score_dtype == torch.float32
+    assert score_args.per_sample_gradient_dtype == torch.float32
+    assert score_args.precondition_dtype == torch.float32
