b6c8be24499aaeedeec9f1ca89f2e8502ac45b69
Juhan Bae <pomonam15@gmail.com>
2024-06-26 13:10:30 -0400

    minor

--- examples/openwebtext/generate.py
+++ examples/openwebtext/generate.py
@@ -1,36 +1,17 @@
 import torch
 import transformers
 
-model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+model_id = "meta-llama/Meta-Llama-3-8B"
 
 pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16, "cache_dir": "./cache"},
-    device="cuda",
+    "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
 )
 
-messages = [
-    {
-        "role": "system",
-        "content": "You are a pirate chatbot who always responds in pirate speak!",
-    },
-    {"role": "user", "content": "Give three tips for staying healthy."},
-]
-
-prompt = pipeline.tokenizer.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True
-)
-
-terminators = [
-    pipeline.tokenizer.eos_token_id,
-    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-]
+prompt = "Three ways to stay healthy are"
 
 outputs = pipeline(
     prompt,
     max_new_tokens=256,
-    eos_token_id=terminators,
     do_sample=True,
     temperature=0.6,
     top_p=0.9,
