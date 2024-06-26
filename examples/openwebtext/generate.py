9b81123f5db9e10a10e056f40cd0709317f8c87d
Juhan Bae <pomonam15@gmail.com>
2024-06-26 14:05:07 -0400

    minor

--- examples/openwebtext/generate.py
+++ examples/openwebtext/generate.py
@@ -2,14 +2,16 @@ import torch
 import transformers
 
 model_id = "meta-llama/Meta-Llama-3-8B"
-
 pipeline = transformers.pipeline(
     "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
 )
 
-# prompt = "Doctors are male and nurses are "
-prompt = "Machine learning can be defined as "
+prompt = "Doctors are male and nurses are"
+# prompt = "Machine learning can be defined as "
 
-outputs = pipeline(prompt)
+outputs = pipeline(prompt, max_length=64)
+print("Prompt:")
+print(prompt)
+print("Prompt + Completion:")
 print(outputs[0]["generated_text"])
 print(outputs[0]["generated_text"][len(prompt):])
