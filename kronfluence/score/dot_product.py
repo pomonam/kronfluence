85f99a573598629b64654861182be3994495cd5c
Juhan Bae <pomonam15@gmail.com>
2024-07-08 16:58:23 -0400

    minor

--- kronfluence/score/dot_product.py
+++ kronfluence/score/dot_product.py
@@ -82,7 +82,7 @@ def compute_dot_products_with_loader(
         for batch in train_loader:
             batch = send_to_device(tensor=batch, device=state.device)
 
-            print(tracked_module_names)
+            print(cached_module_lst[0])
 
             with no_sync(model=model, state=state):
                 model.zero_grad(set_to_none=True)
