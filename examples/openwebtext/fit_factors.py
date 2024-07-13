5e76f3350bda2454c983b0f6f9b5abf5096313a0
Juhan Bae <pomonam15@gmail.com>
2024-07-13 17:41:26 -0400

    Fix bug in loss computation

--- examples/openwebtext/fit_factors.py
+++ examples/openwebtext/fit_factors.py
@@ -24,6 +24,7 @@ def parse_args():
     parser.add_argument(
         "--factors_name",
         type=str,
+        required=True,
         help="Name of the factor.",
     )
     parser.add_argument(
@@ -83,8 +84,6 @@ def main():
     )
     factor_args.covariance_module_partitions = 2
     factor_args.lambda_module_partitions = 4
-
-    # For better numerical precision.
     factor_args.covariance_data_partitions = 4
     factor_args.lambda_data_partitions = 4
     analyzer.fit_all_factors(
--- examples/openwebtext/task.py
+++ examples/openwebtext/task.py
@@ -19,14 +19,12 @@ class LanguageModelingTask(Task):
         logits = model(
             input_ids=batch["input_ids"],
             attention_mask=batch["attention_mask"],
-        ).logits
+        ).logits.float()
         logits = logits[..., :-1, :].contiguous()
         logits = logits.view(-1, logits.size(-1))
-
+        labels = batch["labels"][..., 1:].contiguous()
         if not sample:
-            labels = batch["labels"]
-            shift_labels = labels[..., 1:].contiguous()
-            summed_loss = F.cross_entropy(logits, shift_labels.view(-1), reduction="sum", ignore_index=-100)
+            summed_loss = F.cross_entropy(logits, labels.view(-1), reduction="sum", ignore_index=-100)
         else:
             with torch.no_grad():
                 probs = torch.nn.functional.softmax(logits.detach(), dim=-1)
@@ -34,6 +32,8 @@ class LanguageModelingTask(Task):
                     probs,
                     num_samples=1,
                 ).flatten()
+                masks = labels.view(-1) == -100
+                sampled_labels[masks] = -100
             summed_loss = F.cross_entropy(logits, sampled_labels, ignore_index=-100, reduction="sum")
         return summed_loss
 
@@ -45,7 +45,7 @@ class LanguageModelingTask(Task):
         logits = model(
             input_ids=batch["input_ids"],
             attention_mask=batch["attention_mask"],
-        ).logits
+        ).logits.float()
         shift_labels = batch["labels"][..., 1:].contiguous().view(-1)
         logits = logits[..., :-1, :].contiguous().view(-1, logits.size(-1))
         return F.cross_entropy(logits, shift_labels, ignore_index=-100, reduction="sum")
@@ -53,7 +53,7 @@ class LanguageModelingTask(Task):
     def get_influence_tracked_modules(self) -> List[str]:
         total_modules = []
 
-        # You can uncomment the following lines if you would like to compute influence also on attention layers.
+        # You can uncomment the following lines if you would like to compute influence on attention layers.
         # for i in range(32):
         #     total_modules.append(f"model.layers.{i}.self_attn.q_proj")
         #     total_modules.append(f"model.layers.{i}.self_attn.k_proj")
